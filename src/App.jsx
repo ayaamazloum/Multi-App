4ec1e0c1a22562535ace069d15edff0267cf8d8b
ayaamazloum <ayamazloum.a@gmail.com>
2024-03-27 01:42:22 +0200

    Fetched the api successfully for 4 days

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className="app column">
       <header className="app-header flex row center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cloud"><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-cloud"><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" /></svg>
         <h1 className="white-text">weathery</h1>
       </header>
       <BrowserRouter>
--- src/pages/Weather/index.jsx
+++ src/pages/Weather/index.jsx
@@ -1,6 +1,43 @@
 import "./style.css";
 
+import { useEffect, useState } from "react";
+
 const Weather = () => {
+    const [weather, setweather] = useState([]);
+
+    const [city, setCity] = useState();
+    const [countryCode, setCountryCode] = useState();
+
+    useEffect(() => {
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const formData = new FormData();
+                    formData.append('latitude', position.coords.latitude);
+                    formData.append('longitude', position.coords.longitude);
+                    formData.append('key', 'd3ad8e2bd18d4eadbfe401334be35952');
+                    formData.append('days', 4);
+                    
+                    fetch(`https://api.weatherbit.io/v2.0/forecast/daily?key=d3ad8e2bd18d4eadbfe401334be35952&lat=${position.coords.latitude}&lon=${position.coords.longitude}&days=4`)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                        setCity(data.city);
+                        setCountryCode(data.countryCode);
+                    })
+                    .catch(error => console.error('Error fetching location:', error));
+                },
+                (error) => {
+                    console.error('Error getting location:', error);
+                }
+            );
+        } else {
+            console.error('Geolocation is not supported by this browser.');
+        }
+  }, []);
+
+
     return (
         <div className="weather-page full-height full-width flex center">
             <div className="weather-box rounded flex column center">
