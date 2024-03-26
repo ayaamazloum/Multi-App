e01a27542425503a20632b266728282443a2aeb4
ayaamazloum <ayamazloum.a@gmail.com>
2024-03-27 01:07:59 +0200

    Added the design for weather page

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./styles/utilities.css";
+import "./styles/colors.css";
+import "./styles/index.css";
+
+import Weather from "./pages/Weather";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div className="app column">
+      <header className="app-header flex row center">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-cloud"><path d="M17.5 19H9a7 7 0 1 1 6.71-9h1.79a4.5 4.5 0 1 1 0 9Z" /></svg>
+        <h1 className="white-text">weathery</h1>
+      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/weather" element={<Weather />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Weather/index.jsx
@@ -0,0 +1,43 @@
+import "./style.css";
+
+const Weather = () => {
+    return (
+        <div className="weather-page flex center">
+            <div className="weather-box rounded flex column center">
+                <p>Baalbek</p>
+                <p>date</p>
+                <div className="day-details flex row center">
+                    <div className="flex column center">
+                        <p>icon</p>
+                        <p>weather desc</p>
+                    </div>
+                    <p>temp</p>
+                    <div className="flex column">
+                        <p>details</p>
+                        <p>details</p>
+                        <p>details</p>
+                    </div>
+                </div>
+            <div className="days flex row center">
+                    <div className="day flex column center">
+                        <p>day</p>
+                        <p>icon</p>
+                        <p>temperature</p>
+                    </div>
+                    <div className="day flex column center">
+                        <p>day</p>
+                        <p>icon</p>
+                        <p>temperature</p>
+                    </div>
+                    <div className="day flex column center">
+                        <p>day</p>
+                        <p>icon</p>
+                        <p>temperature</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Weather;
