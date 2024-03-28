316d91129ad970f1b2b0b66d9707842f764e0891
ayaamazloum <ayamazloum.a@gmail.com>
2024-03-28 03:40:55 +0200

    Added note edit functionality

--- src/pages/StickyNotes/index.jsx
+++ src/pages/StickyNotes/index.jsx
@@ -5,24 +5,23 @@ import { useEffect, useState } from "react";
 const StickyNotes = () => {
 
     const [notes, setNotes] = useState();
-    const [newText, setNewtext] = useState();
+    const [newText, setNewText] = useState("");
 
     const addNote = () => {
         setNotes([...notes, { id: notes.length, text: "New Note!", editing: false }]);
         localStorage.setItem('notes', JSON.stringify(notes));
     };
 
-    const startEditing = (id) => {
-        //
-    };
-
-    const stopEditing = (id) => {
-        //
+    const startEditing = (index) => {
+        const newNotes = [...notes];
+        newNotes[index] = { ...newNotes[index], editing: true };
+        setNotes(newNotes);
+        console.log(notes)
     };
 
     const saveNote = (index) => { 
-        const newNotes = notes;
-        newNotes[index] = { id: index, text: newText, editing: true };
+        const newNotes = [...notes];
+        newNotes[index] = { id: index, text: newText, editing: false };
         setNotes(newNotes);
         localStorage.setItem('notes', JSON.stringify(notes));
     };
@@ -45,11 +44,11 @@ const StickyNotes = () => {
             {notes?.map((note, index) => {
                 return (
                     <div key={note.id} className='note'>
-                        {note.editing && <input onChange={(e) => setNewtext(e.target.value)} className='text-input' type='text' defaultValue={note.text} />}
-                        <div className="note-content full-height full-width flex">
-                            <p>{note.text}</p>
+                        <div className="note-content full-height full-width">
+                            {note.editing && <input onChange={(e) => setNewText(e.target.value)} className='text-input' type='text' defaultValue={note.text} />}
+                            {!note.editing && <p>{note.text}</p>}
+                            {!note.editing && <svg onClick={() => startEditing(index)} className="edit-icon lucide lucide-pencil" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" /><path d="m15 5 4 4" /></svg>}
                             {note.editing && <svg onClick={() => saveNote(index)} className="save-icon lucide lucide-check" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M20 6 9 17l-5-5" /></svg>}
-                            {!note.editing && <svg onClick={() => startEditing(note.id)} className="edit-icon lucide lucide-pencil" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" /><path d="m15 5 4 4" /></svg>}
                             <svg onClick={() => deleteNote(note.id)} className="delete-icon lucide lucide-trash" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/></svg>
                         </div>
                     </div>
